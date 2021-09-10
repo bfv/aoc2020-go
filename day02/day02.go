8be1f35c3b02e471d6fd2ca195ca79111467c5be
Bronco Oostermeyer <b.oostermeyer@flusso.nl>
2021-09-10 11:18:36 +0200

    day2b; PwdRule encapsulation

--- day02/day02.go
+++ day02/day02.go
@@ -9,40 +9,48 @@ import (
 )
 
 type PwdRule struct {
-	MinCount int
-	MaxCount int
-	Letter   string
+	i1     int
+	i2     int
+	letter string
 }
 
 func (p *PwdRule) ProcessInput(data []string) {
 	counts := strings.Split(data[0], "-")
-	p.MinCount, _ = strconv.Atoi(counts[0])
-	p.MaxCount, _ = strconv.Atoi(counts[1])
-	p.Letter = data[1][0:1]
+	p.i1, _ = strconv.Atoi(counts[0])
+	p.i2, _ = strconv.Atoi(counts[1])
+	p.letter = data[1][0:1]
 }
 
-func (p PwdRule) Validate(pwd string) bool {
-	count := strings.Count(pwd, p.Letter)
-	return p.MinCount <= count && count <= p.MaxCount
+func (p PwdRule) ValidateCount(pwd string) bool {
+	count := strings.Count(pwd, p.letter)
+	return p.i1 <= count && count <= p.i2
+}
+
+func (p PwdRule) ValidatePosition(pwd string) bool {
+	return (pwd[p.i1-1:p.i1] == p.letter) != (pwd[p.i2-1:p.i2] == p.letter)
 }
 
 func main() {
 	input := aocinput.GetScliceOfStringSlices("input.txt")
-	a := solve(input)
+	a, b := solve(input)
 	fmt.Println("day2a:", a)
+	fmt.Println("day2b:", b)
 }
 
-func solve(input [][]string) int {
-	var answer int
+func solve(input [][]string) (int, int) {
+	var answerA, answerB int
 
 	for i := 0; i < len(input); i++ {
 		rule, pwd := getRuleAndPwd(input[i])
-		if rule.Validate(pwd) {
-			answer++
+		if rule.ValidateCount(pwd) {
+			answerA++
+		}
+		if rule.ValidatePosition(pwd) {
+			answerB++
 		}
 	}
 
-	return answer
+	return answerA, answerB
 }
 
 func getRuleAndPwd(data []string) (PwdRule, string) {
